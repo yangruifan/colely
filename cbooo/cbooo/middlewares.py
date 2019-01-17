72622a70d90dae6310411acabefaa02972e6f99f
ronny <apple@qq.com>
2019-01-17 16:05:27 +0800

    新增

--- cbooo/cbooo/middlewares.py
+++ cbooo/cbooo/middlewares.py
@@ -1,103 +1,92 @@
 # -*- coding: utf-8 -*-
-
-# Define here the models for your spider middleware
-#
-# See documentation in:
-# https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-
-from scrapy import signals
+import random
+from scrapy.contrib.downloadermiddleware.useragent import UserAgentMiddleware
 
 
-class CboooSpiderMiddleware(object):
+import requests
+import re
+import time
+import pymysql
+import redis
+
+
+class ProxyPoolMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
     # passed objects.
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
-    def process_spider_input(self, response, spider):
-        # Called for each response that goes through the spider
-        # middleware and into the spider.
-
-        # Should return None or raise an exception.
-        return None
-
-    def process_spider_output(self, response, result, spider):
-        # Called with the results returned from the Spider, after
-        # it has processed the response.
-
-        # Must return an iterable of Request, dict or Item objects.
-        for i in result:
-            yield i
-
-    def process_spider_exception(self, response, exception, spider):
-        # Called when a spider or process_spider_input() method
-        # (from other spider middleware) raises an exception.
-
-        # Should return either None or an iterable of Response, dict
-        # or Item objects.
-        pass
-
-    def process_start_requests(self, start_requests, spider):
-        # Called with the start requests of the spider, and works
-        # similarly to the process_spider_output() method, except
-        # that it doesn’t have a response associated.
-
-        # Must return only requests (not items).
-        for r in start_requests:
-            yield r
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
-
-
-class CboooDownloaderMiddleware(object):
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the downloader middleware does not modify the
-    # passed objects.
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
+    def __init__(self, ip=''):
+        self.ip = ip
+        self.start_time = int(time.time())
 
     def process_request(self, request, spider):
-        # Called for each request that goes through the downloader
-        # middleware.
+        wait_time = time.time() - self.start_time
+        if wait_time > 5:
+            self.start_time = int(time.time())
+            self.ip = self.get_ip(spider.settings['REDIS_SETTING'])
+        if self.ip != '':
+            spider.logger.info("获取到 ip:" + self.ip)
+            request.meta["proxy"] = "http://" + self.ip
+        else:
+            spider.logger.info("未获取到ip，休眠5秒")
+            time.sleep(5)
 
-        # Must either:
-        # - return None: continue processing this request
-        # - or return a Response object
-        # - or return a Request object
-        # - or raise IgnoreRequest: process_exception() methods of
-        #   installed downloader middleware will be called
-        return None
+    def get_ip(self, config):
+        myredis = redis.Redis(host=config['host'], port=config['port'], db=config['db'], password=config['password'], socket_connect_timeout=5)
+        try:
+            ip = myredis.get("ip")
+            ip = ip.decode("utf-8")
+            return ip
+        except:
+            return ''
 
-    def process_response(self, request, response, spider):
-        # Called with the response returned from the downloader.
 
-        # Must either;
-        # - return a Response object
-        # - return a Request object
-        # - or raise IgnoreRequest
-        return response
+class UserAgentMiddleware(UserAgentMiddleware):
+    def __init__(self, user_agent=''):
+        self.user_agent = user_agent
 
-    def process_exception(self, request, exception, spider):
-        # Called when a download handler or a process_request()
-        # (from other downloader middleware) raises an exception.
+    def process_request(self, request, spider):
+        # 这句话用于随机选择user-agent
+        ua = random.choice(self.user_agent_list)
+        if ua:
+            request.headers.setdefault('User-Agent', ua)
 
-        # Must either:
-        # - return None: continue processing this exception
-        # - return a Response object: stops process_exception() chain
-        # - return a Request object: stops process_exception() chain
-        pass
+    user_agent_list = [
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2226.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2224.3 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
+        "Mozilla/5.0 (X11; OpenBSD i386) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.3319.102 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2309.372 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.2117.157 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1866.237 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/4E423F",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.517 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1623.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.17 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.62 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.2 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1467.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1464.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1500.55 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.90 Safari/537.36",
+        "Mozilla/5.0 (X11; NetBSD) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.60 Safari/537.17",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.15 (KHTML, like Gecko) Chrome/24.0.1295.0 Safari/537.15",
+        "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.14 (KHTML, like Gecko) Chrome/24.0.1292.0 Safari/537.14",
+    ]
 
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
+
--- cbooo/cbooo/pipelines.py
+++ cbooo/cbooo/pipelines.py
@@ -4,8 +4,100 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import pymysql
+from yienmovie.settings import LOCALHOST, USER, PASSWORD, DB, PORT
+import time
 
 
-class CboooPipeline(object):
+class Pipeline(object):
+    def __init__(self):
+        self.db = pymysql.connect(
+            host=LOCALHOST,
+            port=PORT,
+            user=USER,
+            passwd=PASSWORD,
+            db=DB,
+            charset="utf8",
+            use_unicode=True)
+        self.cursor = self.db.cursor()
+        self.dbname = 'Cinemal_from_Company'
+
     def process_item(self, item, spider):
+        if item['sqltype'] == '1':
+            try:
+                self.db.ping()
+            except:
+                self.db = pymysql.connect(
+                    host=LOCALHOST,
+                    port=PORT,
+                    user=USER,
+                    passwd=PASSWORD,
+                    db=DB,
+                    charset="utf8",
+                    use_unicode=True)
+                self.cursor = self.db.cursor()
+            sql = "SELECT * FROM {0} WHERE CompanyID='{1}' and CinemaID='{2}' and OnlineDate='{3}'".format(self.dbname, item['CompanyID'], item['CinemaID'], item['OnlineDate'])
+            # 执行sql语句
+            self.cursor.execute(sql)
+            # 获取所有记录列表
+            results = self.cursor.fetchall()
+            if results == ():
+                # SQL插入语句
+                sql = ("""
+                INSERT INTO {0}(Companyname,CompanyID,CinemaID,CinemaName,Province,City,Area,OnlineDate,Screen,ScreenD,ScreenS,Seat,Tel,Creat_time,source)
+                VALUES ('{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}','{11}','{12}','{13}','{14}','{15}')
+                """.format(self.dbname, item['Companyname'], item["CompanyID"], item["CinemaID"], item["CinemaName"], item["Province"], item["City"], item["Area"], item["OnlineDate"], item["Screen"], item['ScreenD'], item['ScreenS'], item["Seat"], item["Tel"], item['Creat_time'], item['source']))
+                # 执行sql语句
+                self.cursor.execute(sql)            # 提交到数据库执行
+                self.db.commit()
+            else:
+                print('{0}的{1}影院数据已存在。'.format(item['Companyname'], item['CinemaName']))
+
+        return item
+
+
+class Pipelinetwo(object):
+    def __init__(self):
+        self.db = pymysql.connect(
+            host=LOCALHOST,
+            port=PORT,
+            user=USER,
+            passwd=PASSWORD,
+            db=DB,
+            charset="utf8",
+            use_unicode=True)
+        self.cursor = self.db.cursor()
+        self.dbname = 'yien_company_related'
+
+    def process_item(self, item, spider):
+        if item['sqltype'] == '2':
+            try:
+                self.db.ping()
+            except:
+                self.db = pymysql.connect(
+                    host=LOCALHOST,
+                    port=PORT,
+                    user=USER,
+                    passwd=PASSWORD,
+                    db=DB,
+                    charset="utf8",
+                    use_unicode=True)
+                self.cursor = self.db.cursor()
+            sql = "SELECT * FROM {0} WHERE CompanyNameA='{1}' and CompanyNameB='{2}'".format(self.dbname, item['Companyname'], item['CompanyName'])
+            # 执行sql语句
+            self.cursor.execute(sql)
+            # 获取所有记录列表
+            results = self.cursor.fetchall()
+            if results == ():
+                # SQL插入语句
+                sql = ("""
+                INSERT INTO {0}(CompanyNameA,CompanyIdA,CompanyNameB,CompanyIdB,Num,CompanyType,Creat_time,source)
+                VALUES ('{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}')
+                """.format(self.dbname, item['Companyname'], item["CompanyID"], item["CompanyName"], item["CompanyId"], item["Num"], item["CompanyType"], item["Creat_time"], item["source"]))
+                # 执行sql语句
+                self.cursor.execute(sql)            # 提交到数据库执行
+                self.db.commit()
+            else:
+                print('{0}与{1}合作的数据已存在。'.format(item['Companyname'], item['CompanyName']))
+
         return item
